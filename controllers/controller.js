dc82a840b6db79bc98923006e6292ddb603c2d2d
Sara Broad <saraebroad@gmail.com>
2018-06-12 17:32:35 -0400

    working server

---
+++ controllers/controller.js
@@ -0,0 +1,11 @@
+// var db = require("../models");
+// var express = require('express');
+// var request = require('request');
+// var cheerio = require('cheerio');
+// var mongoose = require('mongoose');
+
+// var router = require("express").Router();
+
+// router.get("/", function(req, res){
+//     res.render("home");
+// });
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var request = require("request");
+
+var PORT = process.env.PORT || 8080;
+
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.static("public"));
+
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// var router = require('./controllers/controller.js');
+// app.use('/', router);
+
+
+
+mongoose.connect("mongodb://localhost/NewsScraper");
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
