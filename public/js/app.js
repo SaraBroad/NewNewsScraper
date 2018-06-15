1cd0badb209e29800a6bba05b42fd045c31a7c76
Sara Broad <saraebroad@gmail.com>
2018-06-14 20:22:25 -0400

    handlebars and app changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,33 +3,21 @@ $(document).on('click', '#scrape-btn', function() {
     $.getJSON("/scrape", function (data) {
         // console.log(data);
         for (var i = 0; i < data.length; i++) {
-            $("#articles").append("title-results" + data[i].title + "summary-results" + data[i].summary + "link-results" + data[i].link)
+            $("#articles").append("<div data-id='" + data[i]._id + "'>" + "title-results" + data[i].title + "summary-results" + data[i].summary + "link-results" + data[i].link + "</div>")
         }
     });
 })
 
-//redirect 
-//need link to articles route
-
-
-
-
-
-//   <div class="panel panel-default">
-//     {{#each Article}}
-//     <div class="panel-heading">
-//         <div id="title-results">
-//             {{this.title}}
-//             <button type="submit" class="btn btn-default save-btn">Save Article</button>
-//         </div>
-//     </div>
-//     <div class="panel-body">
-//         <div id="summary-results">
-//             {{this.summary}}
-//         </div>
-//         <div id="link-results">
-//             {{this.link}}
-//         </div>
-//     </div>
-//     {{/each}}
-// </div>
+$(document).on('click', "btn", function(){
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            note: $("input-comment").val();
+        }
+    })
+    .then(function(data){
+        console.log(data);
+     });
+});
